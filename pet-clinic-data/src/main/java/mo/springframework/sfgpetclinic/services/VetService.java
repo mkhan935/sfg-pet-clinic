a516bd5f3b533ed185b472a87fc40902f459d93e
Mohammed Khan <m.khan935@gmail.com>
2018-10-11 14:49:43 -0400

    Added Services, closes #5

---
+++ pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package mo.springframework.sfgpetclinic.services;
+
+import mo.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
