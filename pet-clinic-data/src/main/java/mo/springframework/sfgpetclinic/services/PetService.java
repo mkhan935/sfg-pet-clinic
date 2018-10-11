8101ea17d35f4be765749ee20a56d38a74872f5b
Mohammed Khan <m.khan935@gmail.com>
2018-10-11 15:17:47 -0400

    fixed Issue with code refactoring, closes #11

--- pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package mo.springframework.sfgpetclinic.services;
 
 import mo.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService  extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package mo.springframework.sfgpetclinic.services;
 
 import mo.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
