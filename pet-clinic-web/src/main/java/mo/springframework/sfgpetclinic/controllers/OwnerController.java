fd8b860d77d181b863a567772feef3c694ecf8b7
Mohammed Khan <m.khan935@gmail.com>
2018-10-22 14:11:32 -0400

    fixed links closes #44

--- pet-clinic-web/src/main/java/mo/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/mo/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -18,5 +18,11 @@ public class OwnerController {
     @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
-        return "owners/index";}
+        return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public  String findOwners(){
+        return "not";
+    }
 }
