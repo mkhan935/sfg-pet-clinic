c3a0e074bfe2e03b5bf1c376501a13528f6c19e9
Mohammed Khan <m.khan935@gmail.com>
2018-10-22 13:14:18 -0400

    closes #31

---
+++ pet-clinic-data/src/main/java/mo/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package mo.springframework.sfgpetclinic.services;
+
+import mo.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
